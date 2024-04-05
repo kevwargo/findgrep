9af4fd66b7d30e9b3433a8445d5307b105ab35e6
Jaraš Lojka <jaras.lojka@nordcloud.com>
2024-04-05 19:08:59 +0200

    Make validation more consistent

--- config/flag.go
+++ config/flag.go
@@ -3,7 +3,6 @@ package config
 import (
 	"fmt"
 	"slices"
-	"strconv"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -40,38 +39,22 @@ func (f *flag) String() string {
 }
 
 func (f *flag) Set(raw string) error {
-	switch f.o.CustomType {
-	case "":
-		if raw != "true" {
-			return fmt.Errorf("internal error: unexpected bool value: %q", raw)
-		}
-		f.o.Value = true
-	case "str":
-		f.o.Value = raw
-	case "int":
-		x, err := strconv.Atoi(raw)
-		if err != nil {
-			return err
-		}
-		f.o.Value = x
-	default:
-		// shouldn't happen, as the types are validated when unmarshalling
-		return fmt.Errorf("invalid option type %q", f.o.CustomType)
+	v, err := f.o.setValueFn(raw)
+	if err != nil {
+		return err
 	}
 
 	if f.o.AllowedValues != nil {
-		if !slices.Contains(f.o.AllowedValues, f.o.Value) {
-			return fmt.Errorf("%q is not one of the allowed %v", f.o.Value, f.o.AllowedValues)
+		if !slices.Contains(f.o.AllowedValues, v) {
+			return fmt.Errorf("%q is not one of the allowed %v", v, f.o.AllowedValues)
 		}
 	}
+
+	f.o.Value = v
 
 	return nil
 }
 
 func (f *flag) Type() string {
-	if f.o.CustomType == "" {
-		return "bool"
-	}
-
 	return f.o.CustomType
 }
--- config/option.go
+++ config/option.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"slices"
+	"strconv"
 
 	"gopkg.in/yaml.v3"
 )
@@ -23,6 +24,8 @@ type Option struct {
 	Disabled      *bool         `yaml:"disabled"`
 	Pattern       stringOrSlice `yaml:"pattern"`
 	Target        stringOrSlice `yaml:"target"`
+
+	setValueFn func(string) (any, error)
 }
 
 func (o *Option) AppendArgs(args []string, values ...string) []string {
@@ -55,7 +58,7 @@ func (o *Option) AppendArgs(args []string, values ...string) []string {
 }
 
 func (o *Option) isBool() bool {
-	return o.CustomType == ""
+	return o.CustomType == "bool"
 }
 
 func (o *Option) isInverted() bool {
@@ -80,27 +83,38 @@ func (o *Option) merge(src *Option) {
 	}
 }
 
-func (o *Option) validate() (err error) {
-	if o.AllowedValues != nil {
-		switch o.CustomType {
-		case "":
-			err = errors.New(`an explicit type should be specified along with "allowed-values"`)
-		case "str":
-			err = validateType[string](o)
-		case "int":
-			err = validateType[int](o)
-		default:
-			err = fmt.Errorf("invalid option type: %q", o.CustomType)
+func (o *Option) validate() error {
+	switch o.CustomType {
+	case "":
+		o.CustomType = "bool"
+		fallthrough
+	case "bool":
+		if o.AllowedValues != nil {
+			return errors.New(`an explicit type should be specified along with "allowed-values"`)
 		}
+		o.setValueFn = func(_ string) (any, error) { return true, nil }
+		return validateType[bool](o)
+	case "str":
+		o.setValueFn = func(raw string) (any, error) { return raw, nil }
+		return validateType[string](o)
+	case "int":
+		o.setValueFn = func(raw string) (any, error) { return strconv.Atoi(raw) }
+		return validateType[int](o)
+	default:
+		return fmt.Errorf("invalid option type: %q", o.CustomType)
 	}
-
-	return err
 }
 
 func validateType[T any](o *Option) error {
 	for _, v := range o.AllowedValues {
 		if _, ok := v.(T); !ok {
 			return fmt.Errorf(`invalid allowed-value %v(%T) for type %q`, v, v, o.CustomType)
+		}
+	}
+
+	if d := o.Default; d != nil {
+		if _, ok := d.(T); !ok {
+			return fmt.Errorf("invalid default %v(%T) for type %q", d, d, o.CustomType)
 		}
 	}
 
